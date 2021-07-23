47c8806b3e7efcab762d672dccdd24be7a368000
Fencerr <43830774+Fencerr@users.noreply.github.com>
2021-07-23 15:17:55 -0400

    clickable button and kill counter

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-import pygame
+import pygame, Population, gameobjects
 
 pygame.display.init()
-displayInfo = pygame.display.Info()
+pygame.font.init()
 
+arial = pygame.font.SysFont("arial",50)
+
+displayInfo = pygame.display.Info()
 dimensions = (displayInfo.current_w,displayInfo.current_h)
+
 window = pygame.display.set_mode(dimensions,pygame.FULLSCREEN)
 
+
 running = True
+kills = 0
+def increaseCount():
+    global kills
+    kills += 1
+mainClickButton = gameobjects.Button((100,100),(100,100),
+                                     gameobjects.Color((255,255,255)),
+                                     gameobjects.Color((128,128,128)),
+                                     gameobjects.Color((64,64,64)),increaseCount)
+
 while running:
-    for event in pygame.event.get():
+    allEvents = pygame.event.get()
+    mainClickButton.update(allEvents)
+    mainClickButton.render(window)
+    window.blit(arial.render(str(kills),True,(255,255,255)),(500,300))
+    pygame.display.update()
+    window.fill((0,0,0))
+    for event in allEvents:
         if event.type == pygame.QUIT:
             pygame.quit()
             running = False
